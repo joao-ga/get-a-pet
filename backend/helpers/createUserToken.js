7b2fee5ba4169f695cdc556f2fa8bb845797c146
joaogabiazon <jbiazonferreira@gmail.com>
2024-06-17 21:53:15 -0300

    Function to authenticate and do the user login

--- backend/helpers/createUserToken.js
+++ backend/helpers/createUserToken.js
@@ -11,7 +11,8 @@ const createUserToken = async(user, req, res) => {
     // return token
     res.status(200).json({
         message: "Você está autenticado",
-        token: token
+        token: token,
+        userId: user._id,
     })
 
 }
