297db20d57b80489da00f486a900e9124dd3d357
joaogabiazon <jbiazonferreira@gmail.com>
2024-06-17 21:23:50 -0300

    Create user function and authenticate user by token

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,14 +1,10 @@
 const mongoose = require('mongoose')
 
 async function main() {
-    try{
-        mongoose.set("strictQuery", true)
-        await mongoose
-        .connect("mongodb+srv://jbiazonferreira:vQGETdXLaAwX82kL@get-pet.s887is1.mongodb.net/?retryWrites=true&w=majority&appName=get-pet");
-        console.log("Conectado ao banco");
-    } catch(err) {  
-        console.log(err);
-    }
+    await mongoose.connect("mongodb+srv://jbiazonferreira:vQGETdXLaAwX82kL@get-pet.s887is1.mongodb.net/?retryWrites=true&w=majority&appName=get-pet");
+    console.log("Conectado ao banco");
 }
+
+main().catch((err)=> console.log(err))
 
 module.exports = mongoose
--- backend/index.js
+++ backend/index.js
@@ -10,4 +10,8 @@ app.use(express.json())
 app.use(cors({credentials: true, origin: 'http://localhost:3000' }))
 
 // routes
+const UserRoutes = require('./routes/UserRoutes')
+
+app.use('/users', UserRoutes)
+
 app.listen(5001)
