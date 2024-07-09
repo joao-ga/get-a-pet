1ea58676403e5538840e345d86f05c4f179229ea
joaogabiazon <jbiazonferreira@gmail.com>
2024-07-09 19:48:54 -0300

    Navbar and footer done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
+//components
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Container from './components/layout/Container'
+
 // pages
 import Login from './components/pages/auth/Login'
 import Register from './components/pages/auth/Register'
@@ -8,11 +13,15 @@ import Home from './components/pages/Home'
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <Navbar />
+        <Container>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Container>
+      <Footer />
     </Router>
   );
 }
