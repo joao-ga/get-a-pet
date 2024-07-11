d46c9a68667bc01edf7230bd85fdf3d6e7945490
joaogabiazon <jbiazonferreira@gmail.com>
2024-07-11 13:38:16 -0300

    Upload user image and edit

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -1,26 +1,75 @@
+import api from '../../../utils/api'
 import styles from './Profile.module.css'
 import formStyles from  '../../form/Form.module.css'
 import Input from '../../form/Input'
 import { useState, useEffect } from 'react'
+import useFlashMessage from '../../../hooks/useFlashMessage'
+import RoundedImage from '../../layout/RoundedImage'
 
 function Profile() {
     const [user, setUser] = useState({})
+    const [preview, setPreview] = useState()
+    const [token] = useState(localStorage.getItem('token') || '')
+    const {setFlashMessage} = useFlashMessage()
+
+    useEffect(()=>{
+        api.get('/users/checkuser', {
+            headers:{
+                Authorization: `Bearer ${JSON.parse(token)}`
+            }
+        }).then((response)=>{
+            setUser(response.data)
+        })
+
+    }, [token])
 
     function onFileChange(e) {
-
+        setPreview(e.target.files[0])
+        setUser({...user, [e.target.name]: e.target.files[0]})
     }
 
     function handleChange(e) {
-
+        setUser({...user, [e.target.name]: e.target.value})
     }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        let msgType = 'success'
+        
+        const formData = new FormData()
+
+        await Object.keys(user).forEach((key)=>
+            formData.append(key, user[key])
+        )
+
+        const data = await api.patch(`users/edit/${user._id}`, formData, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then((response)=>{
+            return response.data
+        })
+        .catch((err)=>{
+            msgType='error'
+            return err.response.data
+        })
+
+        setFlashMessage(data.message, msgType)
+    }
+    console.log(user.image);
+    console.log(`${process.env.REACT_APP_API}image/users/${user.image}`);
 
     return(
         <section>
             <div className={styles.profile_header}>
                 <h1>Perfil</h1>
-                <p>Preview Imagem</p>
+                {(user.image || preview) && (
+                    <RoundedImage src={preview ? URL.createObjectURL(preview) : `${process.env.REACT_APP_API}image/users/${user.image}`} alt={user.name} />
+                )}
             </div>
-            <form className={formStyles.form_container}>
+            <form onSubmit={handleSubmit} className={formStyles.form_container}>
                 <Input text="imagem" type="file" name="image" handleOnChange={onFileChange} />
                 <Input text="E-mail" type="email" name="email" placeholder="Digite o seu e-mail"  handleOnChange={handleChange} value={user.email || ''} />
                 <Input text="Nome" type="text" name="name" placeholder="Digite o seu nome" handleOnChange={handleChange} value={user.name || ''}/>
